fee1a429b521761035fbc879024b2becc91f3f74
malikobaidurrehmanofficial <malikobaidurrehman6@gmail.com>
2025-09-09 23:57:17 +0500

    add grades program

---
+++ Lab1/Grades.py
@@ -0,0 +1,14 @@
+a=int(input("Enter your numbers from hundred to get grads : "))
+
+if a<50:
+    print("Grades F")
+elif a<=60:
+    print("Grade E")
+elif a<=70:
+    print("Grade D")
+elif a<=80:
+    print("Grade C")
+elif a<=90:
+    print("Grade B")
+else:
+    print("Grades A")
