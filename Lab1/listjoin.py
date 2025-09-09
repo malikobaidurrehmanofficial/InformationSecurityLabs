d9950ee8a074846233e376072d0891ff99c584ff
malikobaidurrehmanofficial <malikobaidurrehman6@gmail.com>
2025-09-10 00:39:20 +0500

    add listjoin.py

---
+++ Lab1/listjoin.py
@@ -0,0 +1,6 @@
+list1=input("Enter first list separated by , : ").split(",")
+list2=input("Enter second list separated by , : ").split(",")
+
+
+joinedList=list1+list2
+print(f"joined list :  {joinedList}")
